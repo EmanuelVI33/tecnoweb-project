530e0bf7538edcf5f9de0a46d9f318765c7d2eee
emanuel <emanuelvaca39@gmail.com>
2024-07-16 11:09:58 -0400

    Servicio de noticias

--- app/Services/Production/NewsService.php
+++ app/Services/Production/NewsService.php
@@ -2,13 +2,53 @@
 
 namespace App\Services\Production;
 
+use App\Models\Production\News;
+
 class NewsService
 {
-    public function __construct()
+    public function __construct(private News $news)
     {
     }
 
-    public function create() {
-        
+    public function getAll()
+    {
+        return $this->news->all();
+    }
+
+    public function getOne(int $id)
+    {
+        return $this->news->findOrFail($id);
+    }
+
+    public function create(array $data): News
+    {
+        try {
+            return $this->news->create($data);
+        } catch (\Exception $th) {
+            throw new \Exception('news-servive.create Error al crear la noticia', 500);
+        }
+    }
+
+    public function update(int $id, array $data): News
+    {
+        try {
+            $news = $this->getOne($id);
+            $news->fill($data);
+            $news->save();
+            return $news;
+        } catch (\Exception $th) {
+            throw new \Exception('news-servive.update Error al actualizar la noticia', 500);
+        }
+    }
+
+    public function delete(int $id): bool
+    {
+        try {
+            $news = $this->getOne($id);
+            $news->delete();
+            return true;
+        } catch (\Exception $th) {
+            throw new \Exception('news-servive.delete Error al eliminar la noticia', 500);
+        }
     }
 }
