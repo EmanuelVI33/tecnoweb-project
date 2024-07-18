75c0750a062ff4414776f4d57ad4923a6871a918
emanuel <emanuelvaca39@gmail.com>
2024-07-17 21:33:44 -0400

    Add

--- public/build/assets/Dashboard-WRvrgcEz.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./app-D0ROFTDy.js";import{A as i}from"./AuthenticatedLayout-4-8fx1hM.js";import"./index-CYbbjuDm.js";import"./transition-CDPvDqU_.js";import"./NavLink-BQ1-lSA8.js";import"./FontSizeDropdown-gB3xfv_5.js";import"./index-FBn3qQOQ.js";function f({auth:o,presenters:r}){return console.log(r),t.jsx(i,{user:o.user})}export{f as default};
